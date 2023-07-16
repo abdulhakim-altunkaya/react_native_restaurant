6a5c29b1baba55897f984110ed862b6fd80a1590
abdulhakim_altunkaya <altunkaya2@gmail.com>
2023-07-16 17:22:14 +0300

    styling input area finished

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,22 +1,34 @@
 import React from "react";
-import {View, Text, StyleSheet} from "react-native";
+import {View, Text, TextInput, StyleSheet} from "react-native";
 import { FontAwesome } from '@expo/vector-icons';
 
 const SearchBar = () => {
   return(
     <View style={styles.inputContainer}>
-        <FontAwesome name="search"size={30}/>
-        <Text>Search Screen</Text>
+        <FontAwesome name="search" style={styles.iconStyle}/>
+        <TextInput style={styles.inputArea}
+        placeholder="Search" />
     </View>
   )
 }
 
 const styles = StyleSheet.create({
   inputContainer: {
+    marginTop: 10,
     backgroundColor: "#d3d3d3",
     height: 50,
     borderRadius: 5,
     marginHorizontal: 15,
+    flexDirection: "row",
+  },
+  inputArea: {
+    flex: 1,
+    fontSize: 18,
+  },
+  iconStyle: {
+    fontSize: 35,
+    alignSelf: "center",
+    marginHorizontal: 11,
   },
 })
 
--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,8 +1,9 @@
-import React from "react";
+import React, {useState} from "react";
 import {View, Text, StyleSheet} from "react-native";
 import SearchBar from "../components/SearchBar";
 
 const SearchScreen = () => {
+  const [term, setTerm] = useState("");
   return(
     <View>
         <SearchBar />
